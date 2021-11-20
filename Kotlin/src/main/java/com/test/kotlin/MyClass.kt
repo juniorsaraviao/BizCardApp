1e2aa6b165de223aa5c3ffb60f6b815f37282e05
juniorsaraviao <junior.saravia.o@uni.pe>
2021-11-19 22:45:30 -0500

    Adding Kotlin module

---
+++ Kotlin/src/main/java/com/test/kotlin/MyClass.kt
@@ -0,0 +1,14 @@
+package com.test.kotlin
+
+class MyClass {
+}
+
+fun main() {
+    val name = "Kotlin"
+
+    println("Hello $name")
+
+    val a = 23
+    val b = 12
+    println(a.toDouble() / b.toDouble())
+}
