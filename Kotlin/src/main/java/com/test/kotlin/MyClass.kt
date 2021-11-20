73e27b1a5f09250ff496bad241fb77b4ea75a49f
juniorsaraviao <junior.saravia.o@uni.pe>
2021-11-19 23:54:58 -0500

    Working with lambdas

--- Kotlin/src/main/java/com/test/kotlin/MyClass.kt
+++ Kotlin/src/main/java/com/test/kotlin/MyClass.kt
@@ -24,4 +24,31 @@ fun main() {
         println(i)
     }
 
+    calculate(10, 20, 5)
+    println("Cat age is ${calculateCatAge(5)}")
+    println(sum(10, 12))
+    printName("Android - Kotlin")
 }
+
+fun calculate(first: Int, last: Int, multipleOf: Int, message: String = "is multiple of") {
+    for(i in first..last){
+        if( i % multipleOf == 0 ){
+            println("$i $message $multipleOf")
+        }
+    }
+}
+
+//fun calculateCatAge(age: Int): Int {
+//    return age * 7
+//}
+
+val calculateCatAge: (Int) -> Int = {catAge -> catAge * 7}
+
+//fun sum(a: Int, b: Int): Int{
+//    return a + b
+//}
+
+val sum: (Int, Int) -> Int = { a, b -> a + b  }
+
+// Lambdas with void
+val printName: (String) -> Unit = { println(it) }
