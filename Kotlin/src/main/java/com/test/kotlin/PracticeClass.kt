6418ebae52d00bab118d4af6c0b7560bdeea3328
juniorsaraviao <junior.saravia.o@uni.pe>
2021-11-20 22:27:49 -0500

    Working with inheritance

--- Kotlin/src/main/java/com/test/kotlin/PracticeClass.kt
+++ Kotlin/src/main/java/com/test/kotlin/PracticeClass.kt
@@ -7,9 +7,21 @@ fun main(){
     println(car.model)
     car.model = "Change"
     println(car.driver)
+
+    // Truck
+
+    val truck = Truck("Truck driver", "Magenta", "F16")
+    truck.drive()
 }
 
-class Car(var driver: String, var color: String, var model: String) {
+// make the class Car open to inherit from it
+class Truck(driver: String, color: String, model: String) : Car(driver, color, model) {
+    override fun drive() {
+        println("Run truck....")
+    }
+}
+
+open class Car(var driver: String, var color: String, var model: String) {
 
     init {
         println("Init Car")
@@ -18,5 +30,10 @@ class Car(var driver: String, var color: String, var model: String) {
 
     constructor(color: String) : this ("Taxi driver", color, "Taxi"){
         println("Construct Car")
+    }
+
+    // make drive open if we can override this function
+    open fun drive(){
+        println("Init run...")
     }
 }
